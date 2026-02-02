e99b5eab4c3d0c9ff9f4a6a7070ef235e2a382f8
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2026-02-02 20:46:02 +0900

    update

--- Python/hitung persenan.py
+++ Python/hitung persenan.py
@@ -3,6 +3,9 @@ class perhitungan:
         return (b - a) / a * 100
 
 while True:
+    judul = input("saham apa?")
     a = float(input("Masukkan nilai dari: "))
     b = float(input("Masukkan nilai sampai: "))
+    print(f"\n====={judul.upper()}=====")
+    print(f"nilai awal: {a}\nnilai akhir: {b}")
     print(f"Hasil: {perhitungan.hitung(a, b):.2f}%")
