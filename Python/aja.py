7b7fd15fcf5b32fa25dec07a8712d88d5b3bb95d
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2026-02-03 07:57:30 +0900

    m

--- Python/aja.py
+++ Python/aja.py
@@ -1,2 +1,10 @@
-import sys
-print(sys.version)
+
+import cv2
+import numpy as np
+
+img = np.zeros((200,200,3), dtype=np.uint8)
+cv2.putText(img, "OpenCV OK", (10,100),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
+
+cv2.imwrite("test_opencv.png", img)
+print("file test_opencv.png dibuat")
