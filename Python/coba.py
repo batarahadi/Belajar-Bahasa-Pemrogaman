7aaaabe2f0c5ae7bbfd3be2860f267e9df993843
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2026-02-21 07:17:23 +0900

    m

--- Python/coba.py
+++ Python/coba.py
@@ -8,9 +8,7 @@ magenta = "\033[95m"
 cyan = "\033[96m"
 putih = "\033[97m"
 tutup = "\033[0m"
-tutupp = "#000000"
 
 
 print(f"{merah}Ini teks berwarna merah{tutup}")
 print(f"{hijau}Ini teks berwarna hijau")
-print(f"{merahp}Ini teks berwarna merah{tutupp}")
