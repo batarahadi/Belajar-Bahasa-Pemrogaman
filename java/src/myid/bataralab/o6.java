3197566b36791f1428558238cc4ff78f073ff24b
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2026-02-21 21:48:04 +0900

    modifited file

--- java/src/myid/bataralab/o6.java
+++ java/src/myid/bataralab/o6.java
@@ -1,6 +1,8 @@
+//  Belajar Java [Dasar] - 06 - Pengenalan Print Dan AlurEksekusi
+
 package myid.bataralab;
 
-public class o6PengenalanPrintDanAlurEksekusi {
+public class o6 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.print("Hello, World!");
---
+++ java/src/myid/bataralab/o7.java
@@ -0,0 +1,8 @@
+//  Belajar Java [Dasar] - 07 - Memahami String[ ] args
+
+
+package myid.bataralab;
+
+public class o7 {
+    
+}
