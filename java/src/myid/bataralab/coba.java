5754d918297a0c8d4a5b21c9f2c5bcfdd7583c4b
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2026-02-21 21:36:12 +0900

    belajar o6PengenalanPrintDanAlurEksekusi

--- java/src/myid/bataralab/coba.java
+++ java/src/myid/bataralab/coba.java
@@ -1,9 +1,11 @@
 package myid.bataralab;
 
-public class mainan {
+public class coba {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Bataralab!");
         System.out.println("salam kenal, saya mainan dari bataralab");
+        System.out.println("saya sedang belajar bahasa pemrograman java");
+        System.out.println("semoga saya bisa menjadi programmer yang handal");
     }
 }
---
+++ java/src/myid/bataralab/o6PengenalanPrintDanAlurEksekusi.java
@@ -0,0 +1,13 @@
+package myid.bataralab;
+
+public class o6PengenalanPrintDanAlurEksekusi {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+        System.out.println("apa itu java");
+        
+        System.out.printf("ini nomor %d", 1);
+        System.out.printf("ini nomor %d \n", 2);
+        System.out.printf("ini nomor %d \n", 3);
+    }
+}
