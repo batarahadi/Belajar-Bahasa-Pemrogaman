9630a1baf0ddb62883a98a3624bd049c16a8ec6e
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2026-04-10 07:50:57 +0900

    m

--- java/utama.java
+++ java/utama.java
@@ -1,14 +1,19 @@
-class dataNode {
-    int data;
-    dataNode next;
+class iniNode {
+    int data; // Menyimpan data
+    iniNode next; //menunjuk ke node berikutnya
 
-    public dataNode(int data) {
-        this.data = data;
+    public iniNode(int a) {
+        this.data = a;
         this.next = null;
     }
 }
 
 public class utama {
     public static void main(String[] args) {
+        iniNode kepala = new iniNode(10); // Membuat node pertama dengan data 10
+        kepala.next = new iniNode(20); // Menambahkan node kedua dengan data 20
+
+        System.out.println("Data pada node pertama: " + kepala.data); // Output: 10
+        System.out.println("Data pada node kedua: " + kepala.next.data); // Output: 20
     }
 }
