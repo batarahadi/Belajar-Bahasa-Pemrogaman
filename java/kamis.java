043ef903d83d17d13e4fed19ab192a8850fa5695
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2026-04-19 23:00:02 +0900

    Refactor main.py to remove linked list implementation and replace with simple addition; add new kamis.java file for basic addition example; create deletion_In_Doubly_Linked_List.java file.

---
+++ java/kamis.java
@@ -0,0 +1,28 @@
+
+
+// class Node {
+//     int data;
+//     Node sebelum, selanjutnya;
+//     Node(int d) { 
+//         nilai = d; }
+// }
+
+public class kamis {
+    // Node head
+
+    // public void hapusNode(Node target) {
+    //     if (head == null || target == null) return;
+
+    //     if (head == target) {
+    //         head = target.selanjutnya;
+    //     }
+
+    //     if (target.sebelumnya != null) { }
+    // }
+    public static void main(String[] args) {
+        int a, b;
+        a = 1;
+        b = 2;
+        System.out.printf("a + b = %d", a + b);
+    }
+}
